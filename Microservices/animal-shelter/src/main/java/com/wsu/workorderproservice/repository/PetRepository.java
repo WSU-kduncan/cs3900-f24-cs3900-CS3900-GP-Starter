529d881bba9a54f9b8835e6f1f5c90fb94376a76
AvaMcintosh <156035049+AvaMcintosh@users.noreply.github.com>
2024-11-10 20:57:28 -0500

    Update PetRepository.java
    Wasn't sure how to do the query

--- Microservices/animal-shelter/src/main/java/com/wsu/workorderproservice/repository/PetRepository.java
+++ Microservices/animal-shelter/src/main/java/com/wsu/workorderproservice/repository/PetRepository.java
@@ -1,5 +1,16 @@
 package com.wsu.workorderproservice.repository;
 
-public class PetRepository {
-    
+import org.springframework.data.domain.Page;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.wsu.workorderproservice.model.Pet;
+
+public interface  PetRepository extends JpaRepository<Pet, Integer> {
+
+    @Query(nativeQuery = true, value = "add sql query")
+
+    Page<Object[]> findBySearch(String search, Pageable pageable);
+
 }
