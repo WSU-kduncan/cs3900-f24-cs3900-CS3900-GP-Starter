fefe4aa72c661569da3cbc48f05750e92a938e76
AvaMcIntosh <your.email@example.com>
2024-11-30 10:58:10 -0500

    Finished base structure Need to add to homepage, application, adopter, staff Need to finish pet

--- Web-Design/Animal-Shelter/src/app/app.routes.ts
+++ Web-Design/Animal-Shelter/src/app/app.routes.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PetComponent } from './pet/pet.component';
 import { ApplicationComponent } from './application/application.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AdopterComponent } from './adopter/adopter.component';
+import { StaffComponent } from './staff/staff.component';
 
 export const routes: Routes = [
     {path: 'pet', component: PetComponent },
     {path: 'application', component: ApplicationComponent},
+    {path: 'home', component: HomepageComponent},
+    {path: 'adopter', component: AdopterComponent},
+    {path: 'staff', component: StaffComponent},
 
 ];
 
