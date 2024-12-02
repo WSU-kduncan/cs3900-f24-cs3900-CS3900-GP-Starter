b6c0a7340ee3a2bb6343da82a4357c5539b9a973
hmcintos <60016832+hmcintos@users.noreply.github.com>
2024-12-02 01:25:13 -0500

    Update staff.component.ts

--- Web-Design/Animal-Shelter/src/app/staff/staff.component.ts
+++ Web-Design/Animal-Shelter/src/app/staff/staff.component.ts
@@ -9,25 +9,6 @@ import { CommonModule } from '@angular/common';
   templateUrl: './staff.component.html',
   styleUrls: ['./staff.component.css'],
 })
-export class StaffComponent implements OnInit {
-  staff = [
-    { id: 1, name: 'Gani', email: 'Gani@example.com', phone: '555-1234', role: 'Manager' },
-    { id: 2, name: 'Hunter', email: 'Hunter@example.com', phone: '555-5678', role: 'Volunteer' },
-  ];
-
-  newStaff = { id: 0, name: '', email: '', phone: '', role: 'Volunteer' };
-
-  constructor() {}
-
-  ngOnInit(): void {}
-
-  addStaff(): void {
-    if (this.newStaff.name && this.newStaff.email && this.newStaff.phone) {
-      this.newStaff.id = this.staff.length + 1;
-      this.staff.push({ ...this.newStaff });
-      this.resetForm();
-    }
-  }
 
   deleteStaff(id: number): void {
     this.staff = this.staff.filter(member => member.id !== id);
