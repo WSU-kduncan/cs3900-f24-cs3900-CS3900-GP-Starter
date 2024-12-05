a27b5acf4157b8ec7b339fc686ccd6597338e3c4
StarboardHook <sagievg@outlook.com>
2024-12-04 19:03:12 -0500

    Accepted changes

--- Microservices/animal-shelter/src/main/java/com/wsu/workorderproservice/WorkOrderProServiceApplication.java
+++ Microservices/animal-shelter/src/main/java/com/wsu/workorderproservice/WorkOrderProServiceApplication.java
@@ -3,11 +3,8 @@ package com.wsu.workorderproservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-<<<<<<< HEAD
+
 @SpringBootApplication
-=======
-@SpringBootApplication()
->>>>>>> 1682d21cf6b5383f0b10cb0c0c15bed870e2826b
 public class WorkOrderProServiceApplication {
 
 	/**
